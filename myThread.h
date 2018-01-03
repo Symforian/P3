6d7c73f9e247979e6c5907afdab72c5d83bfebac
Symforian <adrienkaminski@wp.pl>
2018-01-03 17:57:02 +0100

    Doprecyzowywanie pliku nagłówkowego, rozplanowywanie funkcji

--- myThread.h
+++ myThread.h
@@ -1,6 +1,25 @@
 #include<ucontext.h>
 
-
+//myThread struct
 typedef struct{
+	ucontext context;
 } myThread;
-int myThreadCreate();
+
+// switching context between myThreads
+void schedule();
+
+//Creating new myThread
+int Create_myThread(/*Arg*/);
+
+//Waiting for myThread to finish
+int Join_myThread();
+
+
+/* TO DO:
+// "Pushing" myThread onto semaphore
+int Wait_myThread();
+// "Popping" myThread of semaphore
+int Signal_myThread();
+//Join on all remaining myThreads
+int WaitForAll_myThreads();
+*/
