8c5f686ff70735bf21ddc5c360f9977cb480acc7
Symforian <adrienkaminski@wp.pl>
2018-01-17 17:43:57 +0100

    Praca nad tworzeniem wątków

--- myThread.cpp
+++ myThread.cpp
@@ -1,9 +1,11 @@
 #include<iostream>
 #include<cstdlib>
 #include "myThread.h"
+#define MY_THREAD_STACK_SIZE 1024 * 32 
 //rdy queue
 static myThread All_myThreads [MTHREADS_NUM];
 
+ucontext_t currentThread, mainThread;
 
 void Init_myThreads()
 {
@@ -20,6 +22,8 @@ void schedule()
 
 int Create_myThread(void (*function)(void) )
 {
+	void* newStack;
+	newStack = malloc(MY_THREAD_STACK_SIZE);
 	return 0;
 }
 int Join_myThread()
